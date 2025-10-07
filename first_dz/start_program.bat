c6fcda99a5c4551455b56a51ae8514295768ca33
Kirill <kirill85pav@gmail.com>
2025-10-07 20:48:47 +0300

    add bat file for start program

--- first_dz/start_program.bat
+++ first_dz/start_program.bat
@@ -2,11 +2,12 @@
 chcp 1251 > log
 del log
 
-set MAIN=main.cpp
-set SORT=sort.cpp
-set EXE=main.exe
+set CPP_FILES="main.cpp" "sort.cpp"
+set EXE="main.exe"
 
-set CHARSET="-finput-charset=utf-8 -fexec-charset=windows-1251"
+set CHARSET="-finput-charset=utf-8" "-fexec-charset=windows-1251"
 if exist %EXE% del %EXE%
 
-g++ "%CHARSET" 
+g++ %CHARSET% %CPP_FILES% -o %EXE%
+
+%EXE%
