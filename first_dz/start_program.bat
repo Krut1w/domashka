6d46686c8150bf144d87a40cb226d9a496ffa37d
Kirill <kirill85pav@gmail.com>
2025-10-11 10:54:58 +0300

    change bat file to normal right work version

--- first_dz/start_program.bat
+++ first_dz/start_program.bat
@@ -2,12 +2,12 @@
 chcp 1251 > log
 del log
 
-set CPP_FILES="main.cpp" "sort.cpp"
+set CPP_FILES="main.cpp sort.cpp"
 set EXE="main.exe"
 
-set CHARSET="-finput-charset=utf-8" "-fexec-charset=windows-1251"
+set CHARSET="-finput-charset=utf-8 -fexec-charset=windows-1251"
 if exist %EXE% del %EXE%
 
-g++ %CHARSET% %CPP_FILES% -o %EXE%
+g++ "%CHARSET%" "%CPP_FILES%" -o %EXE%
 
 %EXE%
